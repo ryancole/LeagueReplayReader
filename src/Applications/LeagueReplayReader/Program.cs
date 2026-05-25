3e3b08b0a0408c4eff4dba002e5d2f7c885621b2
Ryan Cole <ryan@rycole.com>
2026-05-25 00:49:48 -0500

    add new replay reader console app

---
+++ src/Applications/LeagueReplayReader/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeagueReplayReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
